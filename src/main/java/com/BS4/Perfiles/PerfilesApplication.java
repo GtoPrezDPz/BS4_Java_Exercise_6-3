039181970b09247c2a9bd76e1b981f803012fd9c
Gustavo Pérez <gustavo.perez@bosonit.com>
2022-02-11 09:16:50 +0100

    BS4_Java_Exercise_6-3

---
+++ src/main/java/com/BS4/Perfiles/PerfilesApplication.java
@@ -0,0 +1,20 @@
+package com.BS4.Perfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class PerfilesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PerfilesApplication.class, args);
+
+	}
+
+
+
+
+}
